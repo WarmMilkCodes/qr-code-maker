72df774a094dca532f6f8ef32dfa1bf3cfb8b689
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2021-09-28 23:06:35 -0500

    Add files via upload

---
+++ qrcodemaker.py
@@ -0,0 +1,13 @@
+import qrcode
+import time
+
+custom_qr = input("Please enter URL:  ")
+
+img = qrcode.make(custom_qr)
+print('Making QR code and saving to directory...')
+file_name_no_period = custom_qr.replace('.','')
+
+file_name = file_name_no_period + '.jpg'
+time.sleep(5)
+img.save(file_name_no_period)
+print('Your QR code has been saved.')
